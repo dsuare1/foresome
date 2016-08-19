bb4ccc11f9ee11c34517e83fbf69c57acbd721b0
Jimmie Jackson <jimmiejackson414@gmail.com>
2016-08-19 17:35:26 -0500

    front end work

---
+++ public/assets/js/background.js
@@ -0,0 +1,16 @@
+// <!-- Activate cloaking device
+    var randnum = Math.random();
+    var inum = 5;
+    var rand1 = Math.round(randnum * (inum - 1)) + 1;
+    images = new Array
+    images[1] = "assets/img/course_pic_1.jpg"
+    images[2] = "assets/img/course_pic_2.jpg"
+    images[3] = "assets/img/course_pic_3.jpg"
+    images[4] = "assets/img/course_pic_4.jpg"
+    images[5] = "assets/img/course_pic_5.jpeg"
+    var image = images[rand1]
+// Deactivate cloaking device -->
+
+// <!-- Activate cloaking device
+    document.write('<body background="' + image + '" text="white">')
+// Deactivate cloaking device -->

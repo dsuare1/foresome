1c9b32af68d3da60b5803ab936d4375b19211ffb
dsuare1 <suarez.derrick@gmail.com>
2016-08-19 17:34:32 -0500

    all routes in centralized controller; needs to be modulerized

---
+++ controllers/newAdmin-controller.js
@@ -0,0 +1,13 @@
+module.exports = function(router, models) {
+    console.log('Controller loaded --> newAdmin controller (newAdmin-controller.js)');
+
+    // router.post('/handicap-submit', function(req, res) {
+    // 		if (!sessionUser) {
+    // 			console.log('no session user');
+    // 			return res.status(401).sent();
+    // 		} else {
+    // 			models.users.findOne({ where: { email: sessionUser }})
+    // 		}
+    // 	});
+
+};
